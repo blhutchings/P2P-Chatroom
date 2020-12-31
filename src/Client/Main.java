97e74441ffcd0352fdea1861d996f7d9c6b9e1e2
Ben Hutchings <vikingghost10@gmail.com>
2020-12-31 13:47:45 -0400

    Made connection from client to server

---
+++ src/Client/Main.java
@@ -0,0 +1,26 @@
+package Client;
+
+import Host.OpenServer;
+
+import java.util.*;
+
+public class Main {
+    private static final Scanner STDIN = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter host or join.");
+        String input = STDIN.nextLine();
+
+        if (input.equals("host")) {
+            Thread serverHost = new Thread(new OpenServer());
+            serverHost.start();
+            System.out.println("Server started.");
+
+        } else if (input.equals("join")) {
+            Client client = new Client(Host.OpenServer.IP, Host.OpenServer.DEFAULT_PORT);
+        }
+    }
+
+
+
+}
---
+++ src/Host/Receiver.java
@@ -0,0 +1,31 @@
+package Host;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Receiver implements Runnable {
+
+    private final Socket serverSocket;
+    private Scanner clientReceive; //Stuff from client
+    private PrintWriter serverOut; //Stuff sent to client
+
+    //Going to retrieve all comms from clients and put in to data base
+
+    public Receiver (Socket serverSocket) throws IOException  {
+        System.out.println("Receiver started");
+        this.serverSocket = serverSocket;
+        clientReceive = new Scanner(serverSocket.getInputStream());
+        serverOut = new PrintWriter(serverSocket.getOutputStream());
+
+    }
+
+    public void run() {
+        while (true) {
+            if (clientReceive.hasNext()) {
+                System.out.println(clientReceive.next());
+            }
+        }
+    }
+}
